420dbd6f415c9b5b85023431c3c69ea36aac2ea3
Conor <bigtednft@gmail.com>
2022-04-24 11:27:25 +0100

    g

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 4096,
+    growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
       { name: "Body" },
